d7d6b73f758065baf08ea88e555c6461ea676bd9
eelcodijkstra <eelco.j.dijkstra@gmail.com>
2022-05-26 10:11:24 +0200

    Add short description and author info

--- setup.py
+++ setup.py
@@ -3,5 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='sphinx_assessment',
     version='0.1.0',
+    description='Assessment questions for Jupyter Book',
+    author='Eelco Dijkstra',
+    author_email='eelco@infvo.nl',     
     packages=find_packages(include=['sphinx_assessment', 'sphinx_assessment.*'])
 )
