e964d8c3dc0daa46f2834d5504da3a0f94af9951
eelcodijkstra <eelco.j.dijkstra@gmail.com>
2022-05-26 10:07:04 +0200

    first version setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sphinx_assessment',
+    version='0.1.0',
+    packages=find_packages(include=['sphinx_assessment', 'sphinx_assessment.*'])
+)
