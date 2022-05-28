aea3bc920b16f5594dea8ba77f958d7a4e99f0d1
eelcodijkstra <eelco.j.dijkstra@gmail.com>
2022-05-28 10:41:02 +0200

    License added

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(
     version='0.1.0',
     description='Assessment questions for Jupyter Book',
     author='Eelco Dijkstra',
-    author_email='eelco@infvo.nl',     
-    packages=find_packages(include=['sphinx_assessment', 'sphinx_assessment.*'])
+    author_email='eelco@infvo.nl',
+    url='https://github.com/eelcodijkstra/sphinx-assessment',
+    packages=find_packages(include=['sphinx_assessment', 'sphinx_assessment.*']),
+    license="MIT"
 )
