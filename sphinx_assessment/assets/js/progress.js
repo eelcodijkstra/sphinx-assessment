a2fd15ddf2442ff7b18d0d574e06f3de3a971952
eelcodijkstra <eelco.j.dijkstra@gmail.com>
2022-06-07 15:01:37 +0200

    login verbeterd

--- sphinx_assessment/assets/js/progress.js
+++ sphinx_assessment/assets/js/progress.js
@@ -1,5 +1,7 @@
-var progress_db; 
+var progress_db = null;
 var progress_context = {};
+var loginForm = null;
+var progress_user = {username: "noname", password: ""};
 
 const progress_log = function (obj) {
     console.log('progress-log' + JSON.stringify(obj));
@@ -8,11 +10,63 @@ const progress_log = function (obj) {
     obj.time = now.toString();
     obj.book = progress_context.booktitle;
     obj.page = progress_context.pagetitle;
-    progress_db.put(obj);
+    obj.user = progress_user.username;
+    if (progress_db !== null) {
+        progress_db.put(obj);
+    } else {
+        console.log("no log DB");
+    }
 }
 
 function progress_login() {
-    alert("login...");
+  document.getElementById("progressLoginForm").style.display = "block";
+}
+
+const login_template = `
+  <form class="form-container">
+    <h1>Login</h1>
+
+    <label for="name"><b>Naam</b></label>
+    <input type="text" placeholder="naam" name="name" required>
+
+    <label for="psw"><b>Wachtwoord</b></label>
+    <input type="password" placeholder="wachtwoord" name="psw" required>
+
+    <button type="submit" class="btn">Login</button>
+    <button type="button" class="btn cancel" onclick="closeLoginForm()">Close</button>
+  </form>
+`;
+
+function closeLoginForm() {
+  document.getElementById("progressLoginForm").style.display = "none";
+}
+
+function submitLoginForm(evt) {
+    const form = evt.target;
+    console.log(form.name.value + "-" + form.psw.value);
+    progress_user = {username: form.name.value, password: form.psw.value};
+    form.name.value = "";
+    form.psw.value = "";
+    localStorage.setItem('progress_user', JSON.stringify(progress_user));
+    
+    if (progress_user != 'noname') {
+        progress_db = new PouchDB(
+                        'https://ampict.com:6984/testprogress',
+                        {auth: progress_user}
+                      );
+        progress_db.info().then(function (info) {
+          console.log('We have a database: ' + JSON.stringify(info));
+          console.log("pouchDB connected"); 
+        }, function () {
+          console.log("no DB connection");
+          progress_db = null;  
+        });                         
+    } else {
+        progress_db = null;
+    }
+    
+    document.getElementById("progressLoginForm").style.display = "none";    
+    return false;
 }
 
 function progress_init() {
@@ -24,21 +78,52 @@ function progress_init() {
     const topbar = document.querySelector("div.topbar-main");
     var elmt = document.createElement('a');
     elmt.classList.add("full-screen-button");
-    elmt.innerHTML = '<button type="button" class="btn btn-secondary topbarbtn" data-toggle="tooltip" data-placement="bottom" onclick="progress_login()" aria-label="Login" title="" data-original-title="Login">\n    <i class="fas fa-user"></i>\n</button>';
-    topbar.appendChild(elmt);  // komt links, moet eigenlijk rechts   
+    elmt.innerHTML = `
+<button type="button" class="btn btn-secondary topbarbtn" 
+        data-toggle="tooltip" data-placement="bottom" 
+        onclick="progress_login()" aria-label="Login" 
+        title="" data-original-title="Login">    
+  <i class="fas fa-user"></i>
+</button>
+`;
+    topbar.appendChild(elmt);
+    
+    const formDiv = document.createElement('div');
+    formDiv.id = 'progressLoginForm';
+    formDiv.classList.add('form-popup');
+    formDiv.innerHTML = login_template;
+    loginForm = formDiv.querySelector('form');
+    loginForm.onsubmit = submitLoginForm;
+    document.body.appendChild(formDiv);
 }
 
 document.addEventListener('DOMContentLoaded', function(event) {
   console.log("start progress");
   progress_init();
-  progress_db = new PouchDB(
-                  'https://ampict.com:6984/testprogress',
-                  {auth: {username: 'hans', password: 'appelscha1399#Zeekoe'}}
-                );
-  progress_db.info().then(function (info) {
-      console.log('We have a database: ' + JSON.stringify(info));
-  });                         
-  console.log("pouchDB connected?");  
+
+  const stored_user = localStorage.getItem("progress_user");
+  if (stored_user == null) {
+      progress_user = {username: 'noname', password: ""};
+  } else {
+      progress_user = JSON.parse(stored_user);
+  }
+    
+  if (progress_user != 'noname') {
+      progress_db = new PouchDB(
+                      'https://ampict.com:6984/testprogress',
+                      {auth: progress_user}
+                    );
+      progress_db.info().then(function (info) {
+        console.log('We have a database: ' + JSON.stringify(info));
+        console.log("pouchDB connected"); 
+      }, function () {
+          console.log("no DB connection");
+          progress_db = null;          
+      });                         
+  } else {
+      progress_db = null;
+  }
+ 
   if (typeof assessment_log !== 'undefined') { // check if variable exists
         assessment_log = progress_log;
   }    
