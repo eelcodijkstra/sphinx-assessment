cfe977c849a9e1a89a01b01a0b6bd8912eb5627d
eelcodijkstra <eelco.j.dijkstra@gmail.com>
2022-07-11 15:08:44 +0200

    Move to version 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sphinx_assessment',
-    version='0.1.0',
+    version='0.1.1',
     description='Assessment questions for Jupyter Book',
     author='Eelco Dijkstra',
     author_email='eelco@infvo.nl',
