6fe28bbc70cfd5c5533f270a95d7bfe19e7f45e6
eelcodijkstra <eelco.j.dijkstra@gmail.com>
2023-04-01 13:49:58 +0200

    css adapter to new JB css

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sphinx_assessment',
-    version='0.1.4',
+    version='0.1.5',
     description='Assessment questions for Jupyter Book',
     author='Eelco Dijkstra',
     author_email='eelco@infvo.nl',
